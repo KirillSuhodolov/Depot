64f9655ac5705cbfd12cfa22613a2722c8ed476f
KirillSuhodolov <stalin_iosiff@mail.ru>
2012-10-07 16:50:34 +0300

    admin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,15 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com" ,
+    port: 3000,
+    domain: "0.0.0.0",
+    authentication: "plain",
+    user_name: "KirillSuhodolov",
+    password: "oblivion1",
+    enable_starttls_auto: true
+  }
 end
